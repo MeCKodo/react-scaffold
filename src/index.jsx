bfb8ce8cdbd7ee49f0956237463b1ba6c53ef926
kodo <524580860@qq.com>
2017-09-23 14:10:15 +0800

    add: hot reload index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+const rootEl = document.getElementById('root');
+const render = (Component) =>
+  ReactDOM.render(
+    <AppContainer>
+      <Component/>
+    </AppContainer>,
+    rootEl
+  );
+
+render(App);
+if (module.hot) {
+  module.hot.accept('./App', () => render(App));
+}
